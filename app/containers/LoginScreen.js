1f60f90e6c471ce27561c4f8c08a08993b527727
sendeveloper <lightmorning46@gmail.com>
2018-06-10 23:53:49 +0200

    Updated

--- app/containers/LoginScreen.js
+++ app/containers/LoginScreen.js
@@ -4,9 +4,14 @@ import {
   Container,
   Content,
   Form,
+  Footer,
+  FooterTab,
   Item,
   Label,
   Input,
+  Left,
+  List,
+  ListItem,
   Text,
   Button,
   View,
@@ -20,11 +25,43 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as loginActions from '../actions/loginActions';
 import { Actions } from 'react-native-router-flux';
+import { StyleSheet } from 'react-native';
 import { translate } from '../i18n';
 import Messages from '../components/Messages';
 import Header from '../components/Header';
 import Spacer from '../components/Spacer';
 import Logo from '../images/logo.png'
+
+const styles = StyleSheet.create({
+	content: {
+  	padding: 44,
+  	backgroundColor: '#333'
+  },
+  button: {
+  	paddingTop: 2,
+  	paddingBottom: 2,
+    backgroundColor: 'rgba(255,255,255,0.2)',
+    height: 41,
+    fontSize: 18,
+    lineHeight: 21,
+    color: '#FFF',
+  },
+  transparentButton: {
+  	fontSize: 18,
+    lineHeight: 21,
+    backgroundColor: 'transparent',
+    color: '#FFF',
+    textAlign: 'center',
+  },
+  input: {
+  	paddingLeft: 0, 
+  	paddingRight: 0,
+  	color: '#FFF',
+  	fontSize: 16,
+  	lineHeight: 19,
+  },
+
+})
 
 class LoginScreen extends Component {
 	static propTypes = {
@@ -41,7 +78,6 @@ class LoginScreen extends Component {
     const { email, password } = this.state;
 
     // console.log(email, password, this.props.actions);
-
     this.props.actions.loginRequest(email, password);
     //Actions.main();
   };
@@ -50,74 +86,59 @@ class LoginScreen extends Component {
     const { email, password } = this.state;
     const { user } = this.props;
     const locale = 'en';
-    
     return (
       <Container>
-				<Content>
-					<View padder>
-						<Spacer size={25} />
-						<Body>
-							<Thumbnail source={require('../images/logo.png')} />
-						</Body>
-					</View>
-					<View padder>
-	          <Body >
-	            <H2 block>Welcome to</H2>
-	            <H1 block>Upper Network!</H1>
-						</Body>
+				<Content style={styles.content}>
+					<View>
+						<Spacer size={41} />
+						<Thumbnail 
+							style={{ alignSelf: "center" }} 
+							source={require('../images/logo.png')} />
 						<Spacer size={40} />
 						<Body>
-	            <Item regular>
+	            <Item>
 	              <Input
+	              	style={styles.input}
 	                placeholder="Email"
 	                onChangeText={email => this.setState({email})}
-	                value={email}
-	              />
+	                value={email} />
 	            </Item>
 							<Spacer size={10} />
-	            <Item regular>
+	            <Item >
 	              <Input
+	              	style={styles.input}
 	                placeholder="Password"
 	                secureTextEntry={true}
 	                onChangeText={password => this.setState({password})}
-	                value={password}
-	              />
+	                value={password} />
 	            </Item>
+	            <Spacer size={10} />
 	            {user.errorMessage && <Messages message={user.errorMessage} />}
 							<Spacer size={20} />
-						</Body>
-						<Body>
-							<View padder>
-								<Button onPress={this.loginEmail}>
-									<Text>{translate('Login', locale)}</Text>
-								</Button>
-							</View>
-							<View style={{flex: 1}} />
-							<View padder>
-	              <Text onPress={() => Actions.push('forgotPassword')}>
-									{translate('ForgotPassword', locale)}
-								</Text>
-		          </View>
-							<View padder>
-								<Button onPress={() => Actions.push('signUp')}>
-									<Text>{translate('Signup', locale)}</Text>
-								</Button>
-							</View>
+							<Button 
+								full 
+								onPress={this.loginEmail}
+								style={styles.button} >
+								<Text>{translate('Login', locale)}</Text>
+							</Button>
+							<Spacer size={40} />
+							<Button 
+								full
+								style={styles.transparentButton} 
+								onPress={() => Actions.push('forgotPassword')} >
+								<Text>{translate('ForgotPassword', locale)}</Text>
+							</Button>
+							<Button 
+								full
+								style={styles.button} 
+								onPress={() => Actions.push('signUp')} >
+								<Text>{translate('Signup', locale)}</Text>
+							</Button>
 						</Body>
 					</View>
         </Content>
       </Container>
     );
-  }
-}
-
-const styles = {
-  button: {
-    backgroundColor: "#2F7E78",
-    borderRadius: 0,
-    width: 150,
-    margin: 12,
-    alignSelf: 'center'
   }
 }
 

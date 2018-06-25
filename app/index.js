09e34fea9d82ed9a2678f083b30c1542f3e0fa7f
sendeveloper <lightmorning46@gmail.com>
2018-06-25 09:24:53 -0700

    Removed unnecessary code

--- app/index.js
+++ app/index.js
@@ -3,7 +3,6 @@ import React, {Component} from 'react';
 import {Navigator} from 'react-native';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
-// import UpperNetwork from './UpperNetwork';
 import Routes from './routes/index';
 import { StyleProvider } from 'native-base';
 import getTheme from '../native-base-theme/components';
